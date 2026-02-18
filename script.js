47de976e38b8a93a87ba3eae4f2610216603dcbe
urbexsigma11 <urbex0sigma@gmail.com>
2026-02-18 07:12:28 +0100

    script.js

--- script.js
+++ script.js
@@ -25,23 +25,4 @@ btn.addEventListener('click', () => {
 
 function placeBet(type) {
     alert("Postawiono zakład na: " + type);
-}const display = document.getElementById('display');
-const btn = document.getElementById('spinBtn');
-const options = ["ACCESS GRANTED", "ACCESS DENIED", "SYSTEM ERROR", "REBOOTING", "CRITICAL HIT"];
-
-btn.addEventListener('click', () => {
-    btn.disabled = true;
-    let counter = 0;
-    
-    const interval = setInterval(() => {
-        display.innerText = options[Math.floor(Math.random() * options.length)];
-        counter++;
-        
-        if (counter > 20) {
-            clearInterval(interval);
-            const final = options[Math.floor(Math.random() * options.length)];
-            display.innerText = ">>> " + final + " <<<";
-            btn.disabled = false;
-        }
-    }, 50);
-});
+}
